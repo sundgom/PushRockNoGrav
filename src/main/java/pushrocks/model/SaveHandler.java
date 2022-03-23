f772285216b96788d75336993ab8bd43f0a7f499
magnsu <magnsu@ntnu.no>
2022-03-23 07:21:12 -0600

    git commit

--- src/main/java/pushrocks/model/SaveHandler.java
+++ src/main/java/pushrocks/model/SaveHandler.java
@@ -1,20 +1,39 @@
 package pushrocks.model;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class SaveHandler implements SaveHandlerInterface {
 
     @Override
     public void save(String fileName, PushRocks pushRocks) throws FileNotFoundException {
-        try (PrintWriter printWriter = new PrintWriter(new File(getFilePath(fileName))) {
-            writer.println(pushRocks.get)
+        
+        // try (PrintWriter printWriter = new PrintWriter(new File(getFilePath(fileName))) {
+        try {
+            PrintWriter printWriter = new PrintWriter(new File(getFilePath(fileName, true)));
+            printWriter.println(pushRocks.toGameToSaveFormat());
+
+            printWriter.flush(); //ensures that the printwriter is done writing to the file
+            printWriter.close(); //closes the file
+            
+
+        }
+        catch (Exception e) {
+            
         }
         
     }
 
     @Override
     public void load(String fileName) throws FileNotFoundException {
-        // TODO Auto-generated method stub
+        Scanner scanner = new Scanner(new File(fileName));
+
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] lineInfo = line.split(",");
+        }
         
     }
     
