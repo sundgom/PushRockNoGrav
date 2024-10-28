591060dec4ce3166537e321ddf01a98429c6f06b
magnsu <magnsu@ntnu.no>
2024-10-28 18:58:37 +0100

    fix build

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 open module TDT4100_project {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.graphics;
+
+  exports pushrock.fxui;
 }
